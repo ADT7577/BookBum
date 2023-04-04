2ed2b7985b55aebad1da0e90a26a202f10fcf1a2
Alexsa Tolentino <adt7577@nyu.edu>
2023-04-04 17:14:44 -0400

    Nil

---
+++ BookBum/BookBum/BookBumApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBumApp.swift
+//  BookBum
+//
+//  Created by Alexsa Tolentino on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BookBumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
