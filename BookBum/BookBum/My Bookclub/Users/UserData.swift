eaed424bdcdae005719ce5ca0d43116d95b84004
Alexsa Tolentino <adt7577@nyu.edu>
2023-05-02 15:31:45 -0400

    Changes

---
+++ BookBum/BookBum/My Bookclub/Users/UserData.swift
@@ -0,0 +1,28 @@
+
+//  Userdata.swift
+//  BookBum
+// ©Code Academy
+//
+
+import Foundation
+@MainActor
+
+
+class UserData: ObservableObject {
+    @Published var users: String = ""
+    
+    init(){
+        Task{
+            await loadUsers()
+        }
+    }
+    
+    func loadUsers() async{
+        do{
+            let users = try await UserFetchingClient.getUsers()
+            self.users = users
+            } catch {
+                print(error)
+            }
+        }
+    }
---
+++ BookBum/BookBum/My Bookclub/Users/UsersView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BookBum
+//
+//
+//
+
+import SwiftUI
+
+struct UsersView: View {
+    @StateObject var userData = UserData()
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text("Raw JSON Data:")
+                ScrollView {
+                    Text(userData.users)
+                }
+            }
+            .padding()
+            .navigationTitle("Random Users")
+        }
+    }
+}
